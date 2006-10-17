7c281e2a223f0ecfc31f7827718ca901923f3a8e
astraw <astraw@e6c0db7a-28da-0310-a2fc-f6ab65d9114a>
2006-10-17 15:02:47 +0000

    moved basic kalman routines to own package
    git-svn-id: https://code.astraw.com/kookaburra/trunk/adskalman@1401 e6c0db7a-28da-0310-a2fc-f6ab65d9114a

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import os, sys
+
+from kookutils import get_svnversion_persistent
+version_str = '0.3.dev%(svnversion)s'
+version = get_svnversion_persistent(
+    os.path.join('adskalman','version.py'),
+    version_str)
+
+setup(name='adskalman',
+      version=version,
+      description='Kalman filtering routine',
+      author_email='strawman@astraw.com',
+      zip_safe=True,
+      packages = ['adskalman'],
+      )
